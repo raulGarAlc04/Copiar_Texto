70ce79a2b02705d543085fb56a7184517277a1a7
raulGarAlc04 <raulgarciaalcantara7@gmail.com>
2023-10-09 10:33:21 +0200

    Version 1
    Borrar carpeta innecesaria

--- Unidad 1/clasePersona.kt
+++
@@ -1,8 +0,0 @@
-class Persona(var nombre: String, var edad: Int){
-    fun datosPersona() = println("Su nombre es ${nombre} y tiene ${edad} años")
-}
-
-fun main() {
-    val persona = Persona("Manuel", 20)
-    persona.datosPersona()
-}
